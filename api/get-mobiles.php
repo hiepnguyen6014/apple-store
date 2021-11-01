d9d1c37f19d44b696b8fd4c537657869b501dd42
daihiepnguyen <51900683@student.tdtu.edu.vn>
2021-11-01 11:15:55 +0700

    a

--- api/get-mobiles.php
+++ api/get-mobiles.php
@@ -6,10 +6,21 @@ $json['status'] = true;
 //create arr have 5 attribute
 $data = array();
 for ($i = 0; $i < 100; $i++) {
+    //random number 2 to 10
+    $old_price = rand(2, 10) * 1000000;
+    $price = rand(2, 10) * 1000000;
+    //random string have length 10000
+    $name = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 10000);
+
+
     $data[] = array(
         'id' => $i,
-        'name' => 'name' . $i,
-        'price' => $i * 100
+        'name' => $name,
+        'oldprice' => $old_price,
+        'description' => $name,
+        'rate' => 3.5,
+        'price' => $price,
+        'image' => 'http://localhost/mobile/images/' . $i . '.jpg',
     );
 }
 
