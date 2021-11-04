bd556d9b463bf814f5e1bfd93d594f669d865d8d
daihiepnguyen <51900683@student.tdtu.edu.vn>
2021-11-04 16:24:39 +0700

    home

--- api/get-mobiles.php
+++ api/get-mobiles.php
@@ -7,27 +7,50 @@ $json = array();
 $json['status'] = true;
 //create arr have 5 attribute
 $data = array();
-for ($i = 0; $i < 100; $i++) {
-    //random number 2 to 10
-    $old_price = rand(2, 10) * 1000000;
-    $price = rand(2, 10) * 1000000;
-    //random string have length 10000
-    $name = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 10000);
-
-
-    $data[] = array(
-        'id' => $i,
-        'name' => $name,
-        'oldPrice' => $old_price,
-        'description' => $name,
-        'rate' => 3.5,
-        'price' => $price,
-        'numberRate' => $price,
-        'hotSale' => 1,
-        'image' => 'assets/img/product/phone1.webp'
-    );
-}
-
+$data[] = array(
+    'id' => 'product1',
+    'name' => 'iPhone 11 Pro Max 512GB',
+    'oldPrice' => 25000000,
+    'description' => 'Tặng kèm một em người yêu siêu dễ thương',
+    'rate' => 4.5,
+    'price' => 23000000,
+    'numberRate' => 12,
+    'hotSale' => 1,
+    'image' => 'assets/img/product/phone1.webp'
+);
+$data[] = array(
+    'id' => 'product2',
+    'name' => 'iPhone 12 Pro Max 512GB',
+    'oldPrice' => 35000000,
+    'description' => 'Tặng kèm một em người yêu siêu dễ thương',
+    'rate' => 4.5,
+    'price' => 33000000,
+    'numberRate' => 172,
+    'hotSale' => 0,
+    'image' => 'assets/img/product/phone2.webp'
+);
+$data[] = array(
+    'id' => 'product3',
+    'name' => 'iPhone 13 Pro Max 512GB',
+    'oldPrice' => 45000000,
+    'description' => 'Tặng kèm một em người yêu siêu dễ thương',
+    'rate' => 4.5,
+    'price' => 43000000,
+    'numberRate' => 132,
+    'hotSale' => 0,
+    'image' => 'assets/img/product/phone3.webp'
+);
+$data[] = array(
+    'id' => 'product4',
+    'name' => 'iPhone 13 Pro 512GB',
+    'oldPrice' => 15000000,
+    'description' => 'Tặng kèm một em người yêu siêu dễ thương',
+    'rate' => 4.5,
+    'price' => 13000000,
+    'numberRate' => 122,
+    'hotSale' => 1,
+    'image' => 'assets/img/product/phone4.webp'
+);
 $json['data'] = $data;
 
 echo json_encode($json);
