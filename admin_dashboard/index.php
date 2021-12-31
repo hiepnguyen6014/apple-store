7339121e5165cbb9b5167328916df05b4223cd79
frgryr2001 <nhanle.lx@gmail.com>
2021-12-31 11:53:03 +0700

    push

--- admin_dashboard/index.php
+++ admin_dashboard/index.php
@@ -1,12 +1,12 @@
-<?php 
-    session_start();
-    if(!isset($_SESSION['user'])){
-      header('Location : ../login.php');
-      exit();
-    }
-    require_once('../conf/conf.php');
-    $getAllUsers = json_decode(getAllUser()) -> data; 
-    
+<?php
+// session_start();
+// if (!isset($_SESSION['user'])) {
+//     header('Location : ../login.php');
+//     exit();
+// }
+// require_once('../conf/conf.php');
+// $getAllUsers = json_decode(getAllUser())->data;
+
 ?>
 
 <head>
@@ -25,16 +25,14 @@
     <!-- top navigation bar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar"
-                aria-controls="offcanvasExample">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
                 <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
             </button>
 
             <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="/">
                 <img width="260px" height="36px" style="object-fit: cover;" src="/assets/img/logo.png" alt="logo">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar"
-                aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar" aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="topNavBar">
@@ -48,9 +46,8 @@
                 </form>
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <?=$_SESSION['name']?> <i class="bi bi-person-fill"></i>
+                        <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= $_SESSION['name'] ?> <i class="bi bi-person-fill"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><a class="dropdown-item" href="#">Thông tin tài khoản</a></li>
@@ -67,7 +64,7 @@
     <!-- offcanvas -->
     <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
         <div class="offcanvas-body p-0">
-        <nav class="navbar-dark">
+            <nav class="navbar-dark">
                 <ul class="navbar-nav">
                     <li>
                         <div class="text-muted small mt-2 fw-bold text-uppercase px-3">
@@ -100,7 +97,9 @@
                             <span>Đơn hàng</span>
                         </a>
                     </li>
-                    <li class="<?php if(isset($_GET['page'])&& $_GET['page']=='user'){echo 'bg-secondary';}?>">
+                    <li class="<?php if (isset($_GET['page']) && $_GET['page'] == 'user') {
+                                    echo 'bg-secondary';
+                                } ?>">
                         <a href="?page=user" class="nav-link nav-link-custom px-3">
                             <span class="me-2"><i class="icon fas fa-user"></i></span>
                             <span>Tài khoản</span>
@@ -192,82 +191,73 @@
           </div>
         </div> -->
             <?php if (isset($_GET['page']) && $_GET['page'] == 'user') { ?>
-            <div class="row">
-                <div class="col-md-12 mb-3">
-                    <div class="card">
-                        <div class="card-header">
-                            <span><i class="bi bi-table me-2"></i></span> Data Table
-                        </div>
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <span><i class="bi bi-table me-2"></i></span> Data Table
+                            </div>
 
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="example" class="table table-striped data-table" style="width: 100%">
-                                    <thead>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="example" class="table table-striped data-table" style="width: 100%">
+                                        <thead>
 
-                                        <tr>
+                                            <tr>
 
-                                            <th style="text-align: center">Name</th>
-                                            <th style="text-align: center">Username</th>
-                                            <th style="text-align: center">Email</th>
-                                            <th style="text-align: center">Phone</th>
-                                            <th style="text-align: center">Address</th>
-                                            <th style="text-align: center">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php foreach ($getAllUsers as $user) {?>
-                                        <tr>
-                                            <td style="text-align: center"><?=$user->name?></td>
-                                            <td style="text-align: center"><?=$user->username?></td>
-                                            <td style="text-align: center"><?=$user->email?></td>
-                                            <td style="text-align: center"><?=$user->phone?></td>
-                                            <td style="text-align: center"><?=$user->address?></td>
-                                            <td style="text-align: center">
-                                                <!-- <a href="" class="btn btn-danger" data-bs-toggle="modal" data-target="#exampleModal">Delete</a> -->
-                                                <a href="" class="btn btn-danger" data-bs-toggle="modal"
-                                                    data-bs-target="#modal-delete-<?=$user->userId?>">
-                                                    Delete
-                                                </a>
+                                                <th style="text-align: center">Name</th>
+                                                <th style="text-align: center">Username</th>
+                                                <th style="text-align: center">Email</th>
+                                                <th style="text-align: center">Phone</th>
+                                                <th style="text-align: center">Address</th>
+                                                <th style="text-align: center">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach ($getAllUsers as $user) { ?>
+                                                <tr>
+                                                    <td style="text-align: center"><?= $user->name ?></td>
+                                                    <td style="text-align: center"><?= $user->username ?></td>
+                                                    <td style="text-align: center"><?= $user->email ?></td>
+                                                    <td style="text-align: center"><?= $user->phone ?></td>
+                                                    <td style="text-align: center"><?= $user->address ?></td>
+                                                    <td style="text-align: center">
+                                                        <!-- <a href="" class="btn btn-danger" data-bs-toggle="modal" data-target="#exampleModal">Delete</a> -->
+                                                        <a href="" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-delete-<?= $user->userId ?>">
+                                                            Delete
+                                                        </a>
 
-                                                <!-- Modal -->
-                                                <div class="modal fade" id="modal-delete-<?=$user->userId?>"
-                                                    tabindex="-1" aria-labelledby="exampleModalLabel"
-                                                    aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel">Xóa tài
-                                                                    khoản</h5>
-                                                                <button type="button" class="btn-close"
-                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                Bạn có muốn xóa tai khoan <p
-                                                                    style="font-weight: 700; display: inline-block;">
-                                                                    <?=$user->username?></p> ra khỏi hệ thống không?
-                                                            </div>
-                                                            <form action="./delete_User.php" method="post"
-                                                                enctype="multipart/form">
-                                                                <div class="modal-footer">
-                                                                    <input type="hidden" name="delete_idUser"
-                                                                        value="<?=$user->userId?>">
-                                                                    <button type="button" class="btn btn-secondary"
-                                                                        data-bs-dismiss="modal">Không</button>
-                                                                    <button type="submit"
-                                                                        class="btn btn-primary">Có</button>
+                                                        <!-- Modal -->
+                                                        <div class="modal fade" id="modal-delete-<?= $user->userId ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h5 class="modal-title" id="exampleModalLabel">Xóa tài
+                                                                            khoản</h5>
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        Bạn có muốn xóa tai khoan <p style="font-weight: 700; display: inline-block;">
+                                                                            <?= $user->username ?></p> ra khỏi hệ thống không?
+                                                                    </div>
+                                                                    <form action="./delete_User.php" method="post" enctype="multipart/form">
+                                                                        <div class="modal-footer">
+                                                                            <input type="hidden" name="delete_idUser" value="<?= $user->userId ?>">
+                                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Không</button>
+                                                                            <button type="submit" class="btn btn-primary">Có</button>
+                                                                        </div>
+                                                                    </form>
+
                                                                 </div>
-                                                            </form>
-
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </div>
-                                                <!-- end Modal -->
-                                            </td>
-                                        </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                    <tfoot>
-                                        <!-- <tr>
+                                                        <!-- end Modal -->
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                        <tfoot>
+                                            <!-- <tr>
                         <th>Name</th>
                         <th>Position</th>
                         <th>Office</th>
@@ -275,17 +265,17 @@
                         <th>Start date</th>
                         <th>Salary</th>
                       </tr> -->
-                                    </tfoot>
-                                </table>
+                                        </tfoot>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <?php 
-        }
-        ?>
+            <?php
+            }
+            ?>
 
         </div>
     </main>
